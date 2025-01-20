3cb44430e79216c334f3b2197b3e6611ea3f9e32
Murilo Pinto Alves Apparecido <murilo.apparecido@gmail.com>
2025-01-19 22:45:43 -0500

    IOS101 Prework App Complete

--- IOS101-prework/ViewController.swift
+++ IOS101-prework/ViewController.swift
@@ -14,6 +14,40 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        func changeColor() -> UIColor{
+                let red = CGFloat.random(in:0...1)
+                let green = CGFloat.random(in:0...1)
+                let blue = CGFloat.random(in:0...1)
 
+                return UIColor(red: red, green: green, blue:blue, alpha: 0.5)
+            }
+        
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+    @IBOutlet weak var jobLabel: UILabel!
+    
+    @IBOutlet weak var universityLabel: UILabel!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBAction func changeTextColor(_ sender: UIButton) {
+        func changeTextColor() -> UIColor{
+                let red = CGFloat.random(in:0...1)
+                let green = CGFloat.random(in:0...1)
+                let blue = CGFloat.random(in:0...1)
+
+                return UIColor(red: red, green: green, blue:blue, alpha: 0.5)
+            }
+        
+        let randomTextColor = changeTextColor()
+        jobLabel.textColor = randomTextColor
+        
+        universityLabel.textColor = randomTextColor
+        
+        nameLabel.textColor = randomTextColor
+
+    }
 }
-
